4506f079238502e0e31daaed98b951037f6a6316
Jhonatan <61719180+Jhonatanmizu@users.noreply.github.com>
2023-11-25 17:23:14 -0300

    :sparkles: created button

--- src/app/common/components/Button/index.tsx
+++ src/app/common/components/Button/index.tsx
@@ -4,17 +4,21 @@ interface Props {
   text: string;
   disabled?: boolean;
   handleClick: () => void;
+  variant?: "success" | "warning" | "failed";
 }
 
 const Button = ({
   text = "",
   disabled = false,
   handleClick = () => {},
+  variant = "success",
 }: Props) => {
   return (
     <button
       onClick={handleClick}
-      className={`bg-success p-1 rounded-lg  text-white hover:duration-500 hover:ease-in-out mb-4 hover:opacity-75 ${
+      className={`
+        ${variant ? `bg-${variant}` : "bg-success"}
+      p-1 rounded-lg  text-white hover:duration-500 hover:ease-in-out mb-4 hover:opacity-75 ${
         disabled ? "opacity-70" : ""
       }`}
     >
