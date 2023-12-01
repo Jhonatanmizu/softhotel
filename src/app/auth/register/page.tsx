1bd5e4f06187c6651960f1ca1077251246a57e5c
jhonatanmizu <natanjesuss20@gmail.com>
2023-12-01 16:18:14 -0300

    :construction: building register logic

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,44 +1,96 @@
 "use client";
-import React from "react";
-import { useFormik } from "formik";
-// Schema
-import { fullRegisterSchema } from "./schema";
+import React, { useState } from "react";
 // Types
 import { IRegisterData } from "./types";
 // Components
 import RegisterFirstStep from "./components/RegisterFirstStep";
 import { ProgressCircle } from "@/app/common";
+// Services
+import authService from "@/services/auth.service";
+// Repositories
+import userRepo from "@/repositories/user.repository";
 
 const initialValues: IRegisterData = {
   email: "",
   password: "",
   confirmPassword: "",
   name: "",
-  cpf: "",
-  phoneNumber: "",
+  contactIdentity: "",
+  contact: "",
   bio: "",
   address: "",
-  city: "",
-  state: "",
+  neighborhood: "",
+  publicPlace: "",
+  complement: "",
+  number: null,
+  photoUrl: "",
   zipCode: "",
-  birthDate: "",
-  addressNumber: null,
 };
 
 const Register = () => {
-  const form = useFormik({
-    validationSchema: fullRegisterSchema,
-    onSubmit(values, formikHelpers) {},
-    initialValues,
-  });
+  const [currentStep, setCurrentStep] = useState<"first" | "second">("first");
+  const [formValues, setFormValues] = useState<IRegisterData>(initialValues);
+
+  const handleRegister = async () => {
+    try {
+      const { email, password } = formValues;
+      const user = await authService.registerWithEmailAndPassword(
+        email,
+        password
+      );
+      const {
+        name,
+        contactIdentity,
+        complement,
+        bio,
+        publicPlace,
+        neighborhood,
+        zipCode,
+        number,
+        address,
+        contact,
+        photoUrl,
+      } = formValues;
+      await userRepo.createUser(user, {
+        photoUrl,
+        email,
+        name,
+        contactIdentity,
+        bio,
+        contact,
+        address: {
+          complement,
+          publicPlace,
+          neighborhood,
+          zipCode,
+          number,
+          address,
+        },
+      });
+    } catch (error) {
+      console.error("Error when we tried to register user", error);
+    }
+  };
+
   return (
     <main className="flex items-center justify-center min-h-screen p-2 ">
-      <section className="w-full ">
+      <section className="w-full bg-white p-2 max-w-screen-sm sm:shadow-xl rounded-2xl">
         <ProgressCircle />
         <h1 className="text-black-alt text-2xl mb-2 font-display">
           Cadastre-se
         </h1>
-        <RegisterFirstStep />
+        {currentStep === "first" && (
+          <RegisterFirstStep
+            submitForm={(values) => {
+              setFormValues((prevValue) => ({
+                ...prevValue,
+                ...values,
+              }));
+              setCurrentStep("second");
+            }}
+          />
+        )}
+        {currentStep === "second" && <></>}
       </section>
     </main>
   );
