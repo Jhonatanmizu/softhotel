d001115eeb5e566e925320296220b3ba7dd72be3
jhonatanmizu <natanjesuss20@gmail.com>
2023-12-03 20:20:13 -0300

    :recycle: fixed imports in Home

--- src/app/common/index.ts
+++ src/app/common/index.ts
@@ -6,6 +6,14 @@ import BenefitCard from "./components/BenefitCard";
 import HotelCard from "./components/HotelCard";
 import Button from "./components/Button";
 import ProgressCircle from "./components/ProggressCircle";
+import DefaultSlider from "./components/Slider";
 
 ///////////////// EXPORTS
-export { FormControlInput, BenefitCard, HotelCard, Button, ProgressCircle };
+export {
+  FormControlInput,
+  BenefitCard,
+  HotelCard,
+  Button,
+  ProgressCircle,
+  DefaultSlider,
+};
