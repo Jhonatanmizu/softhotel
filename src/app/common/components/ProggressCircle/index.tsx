b5566f6f80e63c4c4c8b694b88baeba3af6fa5d0
jhonatanmizu <natanjesuss20@gmail.com>
2023-12-02 22:40:11 -0300

    :sparkles: added progress logic

--- src/app/common/components/ProggressCircle/index.tsx
+++ src/app/common/components/ProggressCircle/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 
-const ProgressCircle = () => {
+interface ItemAction {
+  onClick: () => void;
+}
+interface Props {
+  currentStep: "first" | "second";
+}
+
+const ProgressCircle = ({ currentStep = "first" }: Props) => {
   return (
     <div className="flex items-center justify-center gap-2">
       <div className="w-6 h-6 rounded-full bg-success"></div>
-      <div className="w-6 h-6 rounded-full bg-success opacity-60"></div>
+      <div
+        className={`w-6 h-6 rounded-full bg-success ${
+          currentStep === "first" ? "opacity-60" : ""
+        }`}
+      ></div>
     </div>
   );
 };
