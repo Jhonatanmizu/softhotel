8eafac7040d177b579c9cbcb2da999e2e54b0ee9
jhonatanmizu <natanjesuss20@gmail.com>
2023-12-03 21:48:55 -0300

    :recycle: added handleError

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,9 +4,40 @@ import {
   signInWithEmailAndPassword,
   sendPasswordResetEmail,
   User,
+  AuthError,
 } from "firebase/auth";
 
 class AuthService {
+  public handleFirebaseAuthError(error: AuthError["code"]) {
+    let errorMessage = "";
+
+    switch (error) {
+      case "auth/invalid-credential":
+        errorMessage = "Email ou senha incorretos";
+        break;
+      case "auth/user-not-found":
+        errorMessage = "Usuário não encontrado";
+        break;
+      case "auth/invalid-email":
+        errorMessage = "Email inválido";
+        break;
+      case "auth/wrong-password":
+        errorMessage = "Senha incorreta, tente novamente";
+        break;
+      case "auth/email-already-in-use":
+        errorMessage = "O email já está cadastrado";
+        break;
+      case "auth/weak-password":
+        errorMessage = "Senha fraca, é necessário no mínimo 6 caracteres";
+        break;
+      default:
+        errorMessage = "Ocorreu um erro, tente novamente mais tarde!";
+        break;
+    }
+
+    return errorMessage;
+  }
+
   public async registerWithEmailAndPassword(
     email: string,
     password: string
@@ -42,6 +73,7 @@ class AuthService {
       await sendPasswordResetEmail(auth, email);
     } catch (error) {
       console.error("Error sending password reset email", error);
+      throw error;
     }
   }
 }
