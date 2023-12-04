9d19c65f66369720fb10e1e6d7e11a3ff82fbdcc
jhonatanmizu <natanjesuss20@gmail.com>
2023-12-03 21:48:28 -0300

    :sparkles: added loading to button

--- src/app/common/components/Button/index.tsx
+++ src/app/common/components/Button/index.tsx
@@ -1,11 +1,15 @@
 import React, { ButtonHTMLAttributes } from "react";
-
+// Components
+import Image from "next/image";
+// Images
+import spinner from "../../../common/images/spinner.svg";
 interface Props {
   text: string;
   disabled?: boolean;
   handleClick: () => void;
   variant?: "success" | "warning" | "failed" | "info" | "outline";
   type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
+  isLoading?: boolean;
 }
 
 const Button = ({
@@ -14,6 +18,7 @@ const Button = ({
   handleClick = () => {},
   variant = "success",
   type = "button",
+  isLoading = false,
 }: Props) => {
   return (
     <button
@@ -31,8 +36,17 @@ const Button = ({
         variant === "outline" ? "text-primary" : "text-white"
       }  hover:duration-500 hover:ease-in-out mb-4 hover:opacity-75 ${
         variant === "outline" ? "border-primary " : ""
-      } ${disabled ? "opacity-70" : ""}`}
+      } ${disabled ? "opacity-70" : ""} flex items-center justify-center gap-1`}
     >
+      {isLoading && (
+        <Image
+          src={spinner}
+          alt="spinner"
+          className="bg-none"
+          width={20}
+          height={20}
+        />
+      )}
       {text}
     </button>
   );
