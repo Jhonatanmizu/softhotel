77b9628ec09292709386b317696f8014f28b664d
jhonatanmizu <natanjesuss20@gmail.com>
2023-12-05 18:55:32 -0300

    :sparkles: added load user functionality

--- src/store/auth/auth.store.ts
+++ src/store/auth/auth.store.ts
@@ -1,8 +1,10 @@
 import { create } from "zustand";
 import { toast } from "react-toastify";
+import { redirect } from "next/navigation";
 // Repositories
 import userRepo from "@/repositories/user.repository";
 // Service
+import storageService from "@/services/storage.service";
 import authService from "@/services/auth.service";
 // Types
 import { createUserDTO } from "@/dtos/user.dto";
@@ -21,7 +23,9 @@ interface AuthState {
   isSigned: boolean;
   isLoading: boolean;
   error: any | null;
+  loadUser: () => void;
 }
+
 const initialUserState: IUser = {
   name: "",
   email: "",
@@ -51,8 +55,10 @@ const useAuthStore = create<AuthState>()((set) => ({
       set({ isLoading: true });
       const user = await authService.loginWithEmailAndPassword(email, password);
       const userData = await userRepo.getUserById(user.uid);
+      storageService.setData("user", userData);
       set({ user: userData, isLoading: false, isSigned: true });
       toast("Login realizado com sucesso!");
+      redirect("/");
     } catch (error: any) {
       console.error("Error when we tried to login", error.code);
       const errorMessage = authService.handleFirebaseAuthError(error.code);
@@ -68,10 +74,13 @@ const useAuthStore = create<AuthState>()((set) => ({
         password
       );
       const createdUser = await userRepo.createUser(userAuth, userDTO);
+      storageService.setData("user", createdUser);
       set({ user: createdUser as IUser, isLoading: false, isSigned: true });
+      redirect("/");
     } catch (error: any) {
       console.error("Error creating user", error);
       const errorMessage = authService.handleFirebaseAuthError(error.code);
+      console.error("Error creating user", errorMessage);
       toast(errorMessage);
       set({ error });
     }
@@ -80,6 +89,13 @@ const useAuthStore = create<AuthState>()((set) => ({
     return await authService.recoveryPassword(email);
   },
   logout: () => {},
+  loadUser: () => {
+    set({ isLoading: true });
+    const user = storageService.getData("user");
+    console.log("LOADED USER", user);
+    if (!user) return;
+    set({ user: user as IUser, isLoading: false, isSigned: true });
+  },
 }));
 
 export default useAuthStore;
