9565e16fe3b8b897afb2b55c9868b335d58591a3
jhonatanmizu <natanjesuss20@gmail.com>
2023-12-05 21:56:20 -0300

    :recycle: added loadUser in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import useAuthStore from "@/store/auth/auth.store";
+import { useEffect } from "react";
 //Components
 import HomePage from "./navigation/home";
 
 const Home = () => {
   const loadUser = useAuthStore.getState().loadUser;
-  loadUser();
+  useEffect(() => {
+    loadUser();
+  }, [loadUser]);
+
   return <HomePage />;
 };
 export default Home;
